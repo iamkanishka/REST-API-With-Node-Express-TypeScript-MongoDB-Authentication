322c58837f76a6d8446414ae591e2c91d8d0711f
iamkanishka <kanishkabc123@gmail.com>
2023-10-01 20:59:14 +0530

    Server Setup with Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello Typescript!");
+import express from 'express';
+import cors from 'cors';
+import http from 'http'; 
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+
+
+
+
+const restAPIApp = express();
+
+restAPIApp.use(cors({
+    credentials: true
+}));
+
+restAPIApp.use(compression());
+restAPIApp.use(cookieParser());
+restAPIApp.use(bodyParser.json());
+
+const restAPIAppServer = http.createServer(restAPIApp);
+
+restAPIAppServer.listen(8080, ()=>{
+    console.log('Server running on http://locahost:8080/');
+    
+})
+
+
+
+
