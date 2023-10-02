de1d3135fbff46814cc36e3cfb10852fc6b97b5c
iamkanishka <kanishkabc123@gmail.com>
2023-10-02 14:33:57 +0530

    Created Users Controller and routes with Middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,33 +1,47 @@
-import { getUserBySessionToken } from "db/users";
-import express from "express";
+import express from 'express';
+import { merge, get } from 'lodash';
 
-import { get, merge } from "lodash";
+import { getUserBySessionToken } from '../db/users'; 
 
-export const isAuthenticated = async (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-) => {
+export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   try {
-    const sessionToken = req.cookies["HOTSHOT_TOKEN"];
+    const sessionToken = req.cookies['ANTONIO-AUTH'];
 
     if (!sessionToken) {
-      res.sendStatus(403);
+      return res.sendStatus(403);
     }
 
-   const existingUser =  await getUserBySessionToken(sessionToken);
+    const existingUser = await getUserBySessionToken(sessionToken);
 
-   if(!existingUser){
-    res.sendStatus(403);
-   }
+    if (!existingUser) {
+      return res.sendStatus(403);
+    }
 
-   merge(req,{identity:existingUser});
+    merge(req, { identity: existingUser });
 
-   return next();
-
-
-  } catch (err) {
-    console.log(err);
+    return next();
+  } catch (error) {
+    console.log(error);
     return res.sendStatus(400);
   }
-};
+}
+
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, 'identity._id') as string;
+
+    if (!currentUserId) {
+      return res.sendStatus(400);
+    }
+
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
